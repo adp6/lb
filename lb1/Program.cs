4bdfca9cad108efcf7b3cf3fbf7dd1486a20d0ce
adp6 <mcdegan244@gmail.com>
2022-08-26 09:09:56 +0300

    second commit

--- lb1/Program.cs
+++ lb1/Program.cs
@@ -6,7 +6,7 @@ namespace lb1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
